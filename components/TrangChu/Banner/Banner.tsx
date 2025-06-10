585fc22ee306e8aebd6e1171e831dfbf48e069c2
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-06-10 15:19:19 +0700

    responsive

--- components/TrangChu/Banner/Banner.tsx
+++ components/TrangChu/Banner/Banner.tsx
@@ -27,8 +27,8 @@ export default async function Banner() {
   const apkdownload = data?.apkdownload || "#";
 
   return (
-    <div className="md:bg-[url('/assets/home/banner.jpg')] bg-[url('/assets/home/bannermobile.jpg')] bg-cover min-h-[75vh] md:min-h-screen bg-top md:bg-center flex flex-col justify-between">
-      <div className="hidden bg-white opacity-85 h-[6vw] inset-3 md:flex justify-between px-[6vw]">
+    <div className="bg-[url('/assets/home/banner.jpg')] bg-cover min-h-screen bg-center flex flex-col justify-between">
+      <div className="bg-white opacity-85 h-[6vw] inset-3 flex justify-between px-[6vw]">
         <div className="bg-[url('/assets/home/logo.png')] w-[8vw] bg-contain bg-no-repeat h-[6vw] translate-y-1/3"></div>
         <ul className="flex justify-around text-[#424242] gap-[2vw] items-center text-[1vw] font-bold">
           <Link href="/">
@@ -48,58 +48,27 @@ export default async function Banner() {
           </Link>
         </ul>
       </div>
-      {/* mobile */}
-      <div className="md:hidden h-[10vw] inset-3 flex flex-col justify-items-start px-[1vw] items-center">
-        <div className="flex justify-between w-full items-center text-[#424242] text-[3vw] font-bold">
-          <div className="w-[60px] h-[60px] bg-[url('/assets/home/logo.png')] bg-contain bg-no-repeat"></div>
-          <div className="flex items-center -mt-6 justify-start">
-            <Link href={groupfb} className="scale-50 max-w-[40px]">
-              <div className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-322px_-636px] w-[70px] h-[70px]"></div>
-            </Link>
-            <Link href="/" className="scale-50 max-w-[40px]">
-              <div className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-394px_-636px] w-[70px] h-[70px]"></div>
-            </Link>
-            <Link href={napthe} className="scale-50 max-w-[90px]">
-              <div className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto]  bg-[-820px_-504px] w-[170px] h-[70px]"></div>
-            </Link>
-            <Link href={appstore} className="scale-50 max-w-[120px]">
-              <div className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-648px_-504px] w-[170px] h-[70px]"></div>
-            </Link>
-          </div>
-        </div>
-        <div className="flex justify-between w-[50%] items-start text-[#424242] text-sm font-bold">
-          <Link href="/tin-tuc">
-            <div className="flex items-center justify-center">
-              <TiGift size={24} /> TIN TỨC
-            </div>
-          </Link>
-          <Link href={groupfb}>
-            <div className="flex items-center justify-center">
-              <MdOutlineSupportAgent size={24} /> HỖ TRỢ
-            </div>
-          </Link>
-        </div>
-        <div></div>
-      </div>
+
       {/* Desktop buttons */}
-      <div className="hidden md:flex relative bg-[url('/assets/home/bannerabove.png')] mx-auto w-[600px] h-[170px] bg-contain bg-no-repeat bg-center gap-2 justify-center items-center">
-        <Link href={napthe} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-292px_-117px] w-[180px] h-[130px]"></Link>
-        <div className="w-[240px] h-[90%] flex flex-col justify-center items-center gap-[10px]">
-          <Link href={appstore} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-626px_-406px] w-[240px] h-[60px]"></Link>
-          <Link href={chplay} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-626px_-344px] w-[240px] h-[60px]"></Link>
+      <div className="flex relative bg-[url('/assets/home/bannerabove.png')] p-5  bg-contain bg-no-repeat bg-center gap-2 justify-center items-center">
+        <Link
+          href={napthe}
+          className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:565%_685%] bg-[position:399%_15%] w-[180px] aspect-[18/13]"
+        ></Link>
+        <div className="flex flex-col justify-center items-center gap-y-2.5 w-[180px] aspect-[18/13]">
+          <Link
+            href={appstore}
+            className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:422%_1503%] bg-[position:80%_40.2%] w-full aspect-[18/6]"
+          ></Link>
+          <Link
+            href={chplay}
+            className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:424%_1303%] bg-[position:80%_48%] w-full aspect-[18/6]"
+          ></Link>
         </div>
-        <Link href={apkdownload} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-122px_-504px] w-[120px] h-[130px]"></Link>
-      </div>
-      {/* Mobile buttons */}
-      <div className="md:hidden max-w-[400px] h-[150px] translate-y-1/2 scale-50 mx-auto flex items-center justify-center">
-        <div className="md:hidden flex relative bg-[url('/assets/home/bannerabove.png')] mx-auto w-[600px] h-[170px] bg-contain bg-no-repeat bg-center gap-2 justify-center items-center">
-          <Link href={napthe} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-292px_-117px] w-[180px] h-[130px]"></Link>
-          <div className="w-[240px] h-[90%] flex flex-col justify-center items-center gap-[10px]">
-            <Link href={appstore} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-626px_-406px] w-[240px] h-[60px]"></Link>
-            <Link href={chplay} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-626px_-344px] w-[240px] h-[60px]"></Link>
-          </div>
-          <Link href={apkdownload} className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')] bg-[length:auto] bg-[-122px_-504px] w-[120px] h-[130px]"></Link>
-        </div>
+        <Link
+          href={apkdownload}
+          className="transition duration-300 hover:brightness-110 bg-[url('/assets/home/button.png')]  bg-[length:565%_685%] bg-[position:399%_15%] w-[180px] aspect-[18/13]"
+        ></Link>
       </div>
     </div>
   );
